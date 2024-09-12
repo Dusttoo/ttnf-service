ede8774e2a3060b1b3d11e0e16b3452c0b1a2a4a
Dusty <dusty.mumphrey@gmail.com>
2024-09-12 17:47:59 -0500

    added name to user model and updated admin toolbar

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV !== 'development') {
 }
 
 const apiClient = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: API_BASE_URL || "https://api-dev.texastopnotchfrenchies.com",
     headers: {
         'Content-Type': 'application/json',
     },
