0157114238cd928fe9007b4a54dc647807d905c3
Dusty <dusty.mumphrey@gmail.com>
2024-09-12 23:37:12 -0500

    updated 404 image

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import notFoundImage from '../images/404.png'
-import notFoundImaage2 from '../images/4042.png'
-import notFoundImage3 from '../images/3.png'
 
 const Container = styled.div`
   display: flex;
@@ -75,7 +73,7 @@ const NotFoundPage: React.FC = () => {
   return (
     <Container>
       <Heading>404</Heading>
-      <Image src={notFoundImaage2} alt="404 Illustration" />
+      <Image src={notFoundImage} alt="404 Illustration" />
       <Message>Oops! The page you’re looking for doesn’t exist.</Message>
       <HomeButton to="/">Go to Home</HomeButton>
 
